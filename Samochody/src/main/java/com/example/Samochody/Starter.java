f8b1bec216a69c99b405e367e8cdbf6bc9ef760e
Marcin Gruda <marcingruda1@gmail.com>
2022-02-03 08:10:01 +0100

    Pierwszy commit - Pusty projekt

---
+++ Samochody/src/main/java/com/example/Samochody/Starter.java
@@ -0,0 +1,12 @@
+package com.example.Samochody;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Starter implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("hello");
+    }
+}
